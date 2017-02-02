8a0800e4eefbd4a4b707af0d0b5ef4e8784a35f7
David Davydov <kctrlv@gmail.com>
2017-02-01 23:31:16 -0700

    complete 4

--- 0/04.exs
+++ 0/04.exs
@@ -1,9 +1,28 @@
 defmodule E04 do
-  def largest_palindrome(nums, digits) do
-    if (nums==2 && digits==2) do
-      9009
-    end
+  def largest_palindrome_product(digits) do
+    digit_range(digits)
+      |> Enum.to_list
+      |> list_products
+      |> Enum.filter(fn(i) -> is_pal?(i) end)
+      |> Enum.max
+  end
+
+  defp digit_range(digits) do
+    # Accurate for digits < 16
+    round(:math.pow(10, (digits-1)))..round(:math.pow(10, digits)-1)
+  end
+
+  defp list_products([h|t]) do
+    Enum.map(t, fn(n) -> h*n end) ++ list_products(t)
+  end
+
+  defp list_products([]) do
+    []
+  end
+
+  defp is_pal?(int) do
+    to_string(int) == String.reverse(to_string(int))
   end
 end
 
-IO.puts E04.largest_palindrome(2,2)
+IO.puts E04.largest_palindrome_product(3)
